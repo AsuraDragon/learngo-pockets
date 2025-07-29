8ad069c0988e56b2f901026507508bfc20ca191b
Héctor Manuel Rasura Fragozo <rasurafragozo_@hotmail.com>
2025-07-29 07:39:14 -0700

    Added a greet func to return a defined greeting

--- hello/main.go
+++ hello/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+func greet() string {
+	return "Hello World"
+}
+
 func main() {
-	fmt.Println("Hello world")
+	greeting := greet()
+	fmt.Println(greeting)
 }
--- hello/main_internal_test.go
+++ hello/main_internal_test.go
@@ -1,7 +1,11 @@
 package main
 
-func Example_main() {
-	main()
-	//Output:
-	//Hello World
+import "testing"
+
+func TestGreet(t *testing.T) {
+	want := "Hello World" //#1
+	got := greet()        //#2
+	if got != want {      //#3
+		t.Errorf("expected: %q, got :%q", want, got)
+	}
 }
